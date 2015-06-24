08b8628e093d2c5db879f45c944ca01015b6935d
Dennis Klein <d.klein@gsi.de>
2015-06-24 21:33:06 +0200

    base on the reminder task

---
+++ app/models/mailer.rb
@@ -0,0 +1,51 @@
+puts 'pluginMailer'
+
+class Mailer < ActionMailer::Base
+  def issue_report(user, issues, days)
+    set_language_if_valid user.language
+    @issues = issues
+    @days = days
+    @issues_url = url_for(:controller => 'issues', :action => 'index',
+                                :set_filter => 1, :assigned_to_id => user.id,
+                                :sort => 'due_date:asc')
+    mail :to => user.mail,
+      :subject => l(:mail_subject_reminder, :count => issues.size, :days => days)
+  end
+
+  # Sends issue reports to issue assignees
+  # Available options:
+  # * :days     => how many days in the future to remind about (defaults to 7)
+  # * :tracker  => id of tracker for filtering issues (defaults to all trackers)
+  # * :project  => id or identifier of project to process (defaults to all projects)
+  # * :users    => array of user/group ids who should be reminded
+  def self.issue_reports(options={})
+    days = options[:days] || 7
+    project = options[:project] ? Project.find(options[:project]) : nil
+    tracker = options[:tracker] ? Tracker.find(options[:tracker]) : nil
+    user_ids = options[:users]
+
+    scope = Issue.open.where("#{Issue.table_name}.assigned_to_id IS NOT NULL" +
+      " AND #{Project.table_name}.status = #{Project::STATUS_ACTIVE}" +
+      " AND #{Issue.table_name}.due_date <= ?", days.day.from_now.to_date
+    )
+    scope = scope.where(:assigned_to_id => user_ids) if user_ids.present?
+    scope = scope.where(:project_id => project.id) if project
+    scope = scope.where(:tracker_id => tracker.id) if tracker
+    issues_by_assignee = scope.includes(:status, :assigned_to, :project, :tracker).
+                              group_by(&:assigned_to)
+    issues_by_assignee.keys.each do |assignee|
+      if assignee.is_a?(Group)
+        assignee.users.each do |user|
+          issues_by_assignee[user] ||= []
+          issues_by_assignee[user] += issues_by_assignee[assignee]
+        end
+      end
+    end
+
+    puts issues_by_assignee.inspect
+
+    issues_by_assignee.each do |assignee, issues|
+      issue_report(assignee, issues, days).deliver if assignee.is_a?(User) && assignee.active?
+    end
+  end
+end
--- lib/tasks/issue_reports.rake
+++ lib/tasks/issue_reports.rake
@@ -1,12 +1,30 @@
 desc <<-END_DESC
 Send issue reports via email.
 
+Available options:
+  * days     => number of days to remind about (defaults to 7)
+  * tracker  => id of tracker (defaults to all trackers)
+  * project  => id or identifier of project (defaults to all projects)
+  * users    => comma separated list of user/group ids who should be notified
+
 Example:
-  rake redmine:send_issue_reports RAILS_ENV="production"
+  rake redmine:send_issue_reports days=7 users="1,23,56" RAILS_ENV="production"
 END_DESC
 
 namespace :redmine do
   task :send_issue_reports => :environment do
-    puts 'not yet implemented'
+    # Load monkey patches properly
+    RedmineApp::Application.eager_load!
+    require File.join(Rails.root, "plugins/redmine_issue_reports/app/models/mailer.rb")
+
+    options = {}
+    options[:days] = ENV['days'].to_i if ENV['days']
+    options[:project] = ENV['project'] if ENV['project']
+    options[:tracker] = ENV['tracker'].to_i if ENV['tracker']
+    options[:users] = (ENV['users'] || '').split(',').each(&:strip!)
+
+    Mailer.with_synched_deliveries do
+      Mailer.issue_reports(options)
+    end
   end
 end
