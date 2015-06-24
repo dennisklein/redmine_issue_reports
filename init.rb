36aea06c5d6e3008bb120449466b5929afcfe9f8
Dennis Klein <d.klein@gsi.de>
2015-06-24 17:47:23 +0200

    plugin skeleton

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_issue_reports do
+  name 'Redmine Issue Reports plugin'
+  author 'Dennis Klein <d.klein@gsi.de>'
+  description 'Send issue report emails'
+  version '0.0.1'
+  url 'https://github.com/dennisklein/redmine_issue_reports'
+  author_url 'https://github.com/dennisklein'
+end
