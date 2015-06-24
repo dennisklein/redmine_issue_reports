3dc191b1c0d339f829dd52ab93ffbe872f94ab4c
Dennis Klein <d.klein@gsi.de>
2015-06-24 21:44:12 +0200

    set version constraint
    can probably be relaxed at a later point in time

--- init.rb
+++ init.rb
@@ -5,4 +5,5 @@ Redmine::Plugin.register :redmine_issue_reports do
   version '0.0.1'
   url 'https://github.com/dennisklein/redmine_issue_reports'
   author_url 'https://github.com/dennisklein'
+  requires_redmine :version_or_higher => '2.5.0'
 end
