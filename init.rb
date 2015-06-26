7aa005dcc98b0320e82efad7ff9a797f70da39da
Dennis Klein <d.klein@gsi.de>
2015-06-26 13:36:08 +0200

    support dev versions

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_issue_reports do
   name 'Redmine Issue Reports plugin'
   author 'Dennis Klein <d.klein@gsi.de>'
   description 'Send issue report emails'
-  version '0.0.1'
+  version IssueReports::VERSION.to_s
   url 'https://github.com/dennisklein/redmine_issue_reports'
   author_url 'https://github.com/dennisklein'
   requires_redmine :version_or_higher => '2.5.0'
---
+++ lib/issue_reports/version.rb
@@ -0,0 +1,53 @@
+module IssueReports
+  module VERSION
+    MAJOR = 0
+    MINOR = 0
+    PATCH = 1
+
+    # Retrieve branch from the working copy
+    def self.branch
+      self.git('rev-parse --abbrev-ref HEAD').strip
+    end
+
+    # Retrieves the revision from the working copy
+    def self.revision
+      git_description = self.git 'describe --long --dirty --abbrev=10 --always'
+      if git_description =~ /.*?-\d+-([0-9a-z-]+)/
+        return $1
+      elsif git_description =~ /.*?([0-9a-z]{10})/
+        return $1
+      end
+      nil
+    end
+
+    def self.git(args)
+      cwd = File.dirname(__FILE__)
+      if File.directory?(File.join(cwd, '..', '..', '.git'))
+        begin
+          return Dir.chdir(cwd) { `git #{args}` }
+        rescue
+          # Silently fail
+        end
+      end
+      nil
+    end
+
+    BRANCH = self.branch
+    REVISION = self.revision
+    case BRANCH
+    when 'master'
+      ARRAY = [MAJOR, MINOR, PATCH]
+    else
+      ARRAY = [MAJOR, MINOR, PATCH, BRANCH, REVISION].compact
+    end
+    STRING = ARRAY.join('.')
+
+    def self.to_a
+      ARRAY
+    end
+
+    def self.to_s
+      STRING
+    end
+  end
+end
